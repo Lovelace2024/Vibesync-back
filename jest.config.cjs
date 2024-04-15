5e2e5f9365a7cd730f120a8afd65d8ff2c950572
JuanAzula <141027910+JuanAzula@users.noreply.github.com>
2024-04-15 13:00:54 +0200

    Add jest and redis client

---
+++ jest.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "/prisma/"
+  ],
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+};
