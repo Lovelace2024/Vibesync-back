872ec680b76fdea93bef8b1c990fa1380a769ea7
JuanAzula <141027910+JuanAzula@users.noreply.github.com>
2024-04-16 12:08:55 +0200

    Add tests

--- src/test/login.test.ts
+++ src/test/login.test.ts
@@ -1,11 +1,11 @@
-// import app from "../app.ts";
-// import request from "supertest";
+import app from "../server.ts";
+import request from "supertest";
 
-// test('login', async () => {
-//     const mockRequest = {
-//         body: { username: "admin@example.com", password: "123456K*" }
-//     }
-// const login = await request(app).post('/api/login').send(mockRequest.body)
-// console.log(login)
-//     expect(login.status).toBe(200)
-// })
+test('login', async () => {
+    const mockRequest = {
+        body: { username: "admin@example.com", password: "123456K*" }
+    }
+    const login = await request(app).post('/api/login').send(mockRequest.body)
+    console.log(login)
+    expect(login.status).toBe(200)
+})
