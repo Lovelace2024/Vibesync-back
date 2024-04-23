43ef72f04d410c438709ebcea79637962b0f657d
Alba Martin <albams51289@gmail.com>
2024-04-23 14:08:12 +0200

    Album controllers edited due to changes on schema

--- src/controllers/albums.controller.ts
+++ src/controllers/albums.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
 import prisma from "../db/client.ts"
 
+//Para hacer el post hay que poner en el json genre y artist (no genreName y artistName)
 export const addAlbum = async (req: Request, res: Response) => {
     const { name, thumbnail, genre, artist, tracks } = req.body
     try {
@@ -59,48 +60,120 @@ export const getAlbum = async (req: Request, res: Response) => {
             res.status(404).json({ message: "Album not found" });
         }
 
-        res.status(200).send(selectedAlbum);
+        res.status(200).send({
+            msg: "Selected Album",
+            data: selectedAlbum
+          });
 
     } catch (error) {
-        res.status(500).json({ message: "Internal server error" })
+        res.status(400).send(error)
     }
 }
 
+//Para hacer el patch hay que poner en el json genreName y artistName
 export const updateAlbum = async (req: Request, res: Response) => {
     const { albumId } = req.params
     const { name, thumbnail, genre, artist, tracks } = req.body
 
-    try {
-        const updatedAlbum = await prisma.albums.update({
-            where: {
-                id: albumId
-            },
-            data: {
-                name,
-                thumbnail,
-                genre,
-                artist,
-                tracks
-            }
+    if(!albumId) {
+        return res.status(400).send({
+            msg: "The fiel albumId is required"
         })
-        res.status(201).send(updatedAlbum)
+    }
+
+    try {
+        const updatedAlbum = await prisma.$transaction(async (prisma) => {
+            const updatedAlbum = await prisma.albums.update({
+                where: {
+                    id: albumId
+                },
+                data: {
+                    name,
+                    thumbnail,
+                    genre,
+                    artist
+                }
+            })
+            if (tracks && tracks.length > 0) {
+                await prisma.tracksOnAlbums.deleteMany({
+                    where: {
+                        albumId: albumId
+                    }
+                })
+
+                const createTracks = tracks.map((trackId: number) => ({
+                    albumId: albumId,
+                    trackId: trackId
+                }))
+                await prisma.tracksOnAlbums.createMany({
+                    data: createTracks
+                })
+            }
+            return prisma.albums.findUnique({
+                where: {
+                    id: albumId
+                },
+                include: {
+                    tracks: true
+                }
+            })
+        })
+        res.status(200).send({
+            msg: "Album updated successfully",
+            data: updatedAlbum,
+            type: typeof updatedAlbum
+        })
     } catch (error) {
-        res.status(500).json({ message: "Internal server error " + error })
+        res.status(400).send(error)
     }
 }
+
 
 export const deleteAlbum = async (req: Request, res: Response) => {
     const { albumId } = req.params
 
+    if(!albumId){
+        return res.status(400).send({
+            msg: "The field albumId is required"
+        })
+    }
+
     try {
-        const deletedAlbum = await prisma.artists.delete({
+        const album = await prisma.albums.findUnique({
             where: {
                 id: albumId
+            },
+            include: {
+                tracks: true
             }
         })
-        res.status(201).json({ message: "Album deleted successfully" })
+
+        if(!album){
+            return res.status(400).send({
+                msg: "Album not found"
+            })
+        }
+
+        const deletedAlbum = await prisma.$transaction(async(prisma)=> {
+            await prisma.tracksOnAlbums.deleteMany({
+                where: {
+                    albumId: albumId
+                }
+            })
+            const deletedAlbum = await prisma.albums.delete({
+                where: {
+                    id: albumId
+                }
+            })
+            return deletedAlbum
+        })
+        res.status(200).send({
+            msg: "Album deleted successfully",
+            data: deletedAlbum,
+            type: typeof deletedAlbum
+        })
 
     } catch (error) {
-        res.status(500).json({ message: "Internal server error" })
+        res.status(400).send(error)
     }
 }
