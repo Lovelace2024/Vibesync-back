cf619c23a1444ff4d407e763c9ac84b4a2825232
MariaGruber <grubermar1992@gmail.com>
2024-04-24 17:37:03 +0200

    playlists controllers working please check endpoints

---
+++ src/routes/playlists.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express"
+import {  addTrackToPlaylist, createPlaylist, deletePlaylist, getAllPlaylists, getPlaylist, removeTrackfromPlaylist, updatePlaylist } from "../controllers/playlists.controller.ts"
+
+const playlistsRoutes = Router()
+
+playlistsRoutes.post("/user/:userId/playlists", createPlaylist)
+playlistsRoutes.get("/user/:userId/playlists", getAllPlaylists)
+playlistsRoutes.get("/user/:userId/playlists/:playlistId", getPlaylist)
+playlistsRoutes.patch("/user/:userId/playlists/:playlistId", updatePlaylist)
+playlistsRoutes.delete("/user/:userId/playlists/:playlistId", deletePlaylist)
+playlistsRoutes.post("/user/:userId/playlists/:playlistId/addTrack", addTrackToPlaylist)
+playlistsRoutes.delete("/user/:userId/playlist/:playlistId/removeTrack", removeTrackfromPlaylist)
+
+
+
+export default playlistsRoutes
