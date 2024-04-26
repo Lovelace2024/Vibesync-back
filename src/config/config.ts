bd0cc0f4d2e2f50a2c701a04ac6bb6c99403a5e9
Alba Martin <albams51289@gmail.com>
2024-04-26 18:57:59 +0200

    Auth0 deleted

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,6 @@ type TConfig = {
 
 type EnvironmentConfig = {
     app: AppConfig;
-    auth0: Auth0Config;
     // db: DBConfig;
 }
 
@@ -14,11 +13,7 @@ type AppConfig = {
     PORT: string | number
 }
 
-type Auth0Config = {
-    client_origin: string | undefined,
-    audience: string | undefined,
-    issuer: string | undefined
-}
+
 
 type DBConfig = {
     URI: string
@@ -40,21 +35,11 @@ const CONFIG: TConfig = {
     development: {
         app: {
             PORT: PORT || 4002
-        },
-        auth0: {
-            client_origin: process.env.APP_ORIGIN,
-            audience: process.env.AUTH0_AUDIENCE,
-            issuer: process.env.AUTH0_ISSUER
         }
     },
     production: {
         app: {
             PORT: PORT || 8082
-        },
-        auth0: {
-            client_origin: process.env.APP_ORIGIN,
-            audience: process.env.AUTH0_AUDIENCE,
-            issuer: process.env.AUTH0_ISSUER
         }
         // db: {
         //     URI: MONGODB_URI || "mongodb://127.0.0.1:27017/vibesync"
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import express, { Express } from 'express'
 import responseTime from 'response-time'
 import morgan from 'morgan'
 import helmet from 'helmet'
-import errorHandler from './middlewares/error.middleware.ts'
 import cors from 'cors'
 import userRoutes from './routes/user.routes.ts'
 import loginRoutes from './routes/login.routes.ts'
@@ -16,7 +15,6 @@ const app: Express = express()
 
 app.use(helmet())
 app.use(morgan('dev'))
-app.use(errorHandler)
 app.use(responseTime())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
